0765fd3a31c688c7eab92a1f16b4c06fa99dac0d
Pavan Prasad <110252432+Pavanmum@users.noreply.github.com>
2024-07-31 23:55:49 +0530

    Refactor code to improve extraction of digits from a given number

---
+++ Extract Digits from A Given Number/Reverse Digits of A Number/index.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+
+// Create an interface for reading input from the console
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Prompt the user for an integer input
+rl.question('Enter an integer: ', (input) => {
+    // Parse the input as an integer
+    let n = parseInt(input);
+    let reverseNumber = 0;
+    while(n > 0){
+        let number = n % 10;
+        reverseNumber = (reverseNumber * 10 )+ number;
+        n = Math.floor(n / 10);
+    }
+    console.log(reverseNumber);
+
+    rl.close();
+});
